22310916fc5423ae10562654cea1a68eab9b60b5
Dorian CARDOSO <doriancardoso00@gmail.com>
2025-10-27 16:33:02 +0100

    feat: update auth and user routes to use validation middleware

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,13 +1,15 @@
 // src/routes/authRoutes.ts
 import { Router } from 'express';
 import { authController } from '../controllers/authController';
+import {validate} from "../middlewares/validate";
+import {loginSchema, refreshTokenSchema, registerSchema} from "../validators/authValidator";
 
 const authRouter = Router();
 
 // Public routes
-authRouter.post('/login', authController.login);
-authRouter.post('/register', authController.register);
-authRouter.post('/refresh', authController.refresh);
+authRouter.post('/login', validate(loginSchema), authController.login);
+authRouter.post('/register', validate(registerSchema), authController.register);
+authRouter.post('/refresh', validate(refreshTokenSchema), authController.refresh);
 authRouter.post('/logout', authController.logout);
 
 export default authRouter;
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -3,14 +3,16 @@ import express from 'express';
 import { userController } from '../controllers/userController';
 import { authenticate } from '../middlewares/authMiddleware';
 import { authorizeRoles } from '../middlewares/authorizeRoles';
+import {validate} from "../middlewares/validate";
+import {createUserSchema, updateUserSchema} from "../validators/userValidator";
 
 const userRouter = express.Router();
 
 // Protected routes
 userRouter.get('/', authenticate, authorizeRoles('ADMIN'), userController.getAllUsers);
 userRouter.get('/:id', authenticate, authorizeRoles('ADMIN'), userController.getUserById);
-userRouter.post('/', authenticate, authorizeRoles('ADMIN'), userController.createUser);
-userRouter.put('/:id', authenticate, authorizeRoles('ADMIN'), userController.updateUser);
+userRouter.post('/', authenticate, authorizeRoles('ADMIN'), validate(createUserSchema), userController.createUser);
+userRouter.put('/:id', authenticate, authorizeRoles('ADMIN'), validate(updateUserSchema), userController.updateUser);
 userRouter.delete('/:id', authenticate, authorizeRoles('ADMIN'), userController.deleteUser);
 
 export default userRouter;
