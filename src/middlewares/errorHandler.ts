ccb514ddb01e21f64f2bfeeb2ffd2c14727cd42c
Dorian CARDOSO <doriancardoso00@gmail.com>
2025-10-28 11:49:13 +0100

    feat(errorHandler): errorHandler now handle custom errors or throws an unknown error

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,5 +1,7 @@
 // src/middlewares/errorHandler.ts
 import { Request, Response, NextFunction } from "express";
+import {HttpError} from "../errors";
+import {ENV} from "../config/env";
 
 export const errorHandler = (
     err: any,
@@ -9,8 +11,19 @@ export const errorHandler = (
 ) => {
     console.error("❌", err);
 
-    const status = err.status || 500;
-    const message = err.message || "Internal server error";
+    if (err instanceof HttpError) {
+        return res.status(err.status).json({
+            status: "error",
+            message: err.message,
+            ...(err.details && { details: err.details }),
+        });
+    }
+
+    const status = 500;
+    const message =
+        ENV.NODE_ENV === "production"
+            ? "Internal server error"
+            : err.message || "Unknown error";
 
     res.status(status).json({
         status: "error",
